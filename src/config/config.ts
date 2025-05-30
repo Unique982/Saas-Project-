326d309e5ac8be12205892aaf9e9de1c466943c7
Unique Neupane <khemrajneupane111@gmail.com>
2025-05-30 21:02:12 +0545

    connection database and create table new

--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-config();
-
-const envConfig = {
-  // create object
-  portNumber: process.env.PORT,
-};
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,13 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 
 const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME, // databaseName
+  username: process.env.DB_USER, // database ko username by default root
+  password: process.env.DB_PASSWORD, // database ko password by default ""
+  host: process.env.DB_HOST, // database ko location kaha xa vanne kura, localhost(mycomputer)
+  dialect: "mysql", // k database use garna
+  port: Number(process.env.DB_PORT), // database ko port number by default 3306 hunxa
+  models: [__dirname + "/models"],
 });
 
 sequelize
@@ -17,4 +18,7 @@ sequelize
   .catch((err) => {
     console.log(err);
   });
+sequelize.sync({ force: true }).then(() => {
+  console.log("migrated successfully new changes");
+});
 export default sequelize;
